7f6ae209d5cb50c2da09a95e55695aaa78ca57be
Andy010 <andy_gates_010@hotmail.co.uk>
2014-09-15 20:54:34 +0100

    Refactored PAIRS in js

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -13,17 +13,30 @@ Player.prototype.picks = function(pick) {
   	// body...
 };
 
+// Game.prototype.PAIRS = {
+// 	rock: { scissors: 'beats', lizard: 'beats' },
+// 	paper: { rock: 'beats', spock: 'beats' },
+// 	scissors: { paper: 'beats', lizard: 'beats' },
+// 	lizard: { spock: 'beats', paper: 'beats' },
+// 	spock: { scissors: 'beats', rock: 'beats' },
+// };
+
 Game.prototype.PAIRS = {
-	rock: { scissors: 'beats', lizard: 'beats' },
-	paper: { rock: 'beats', spock: 'beats' },
-	scissors: { paper: 'beats', lizard: 'beats' },
-	lizard: { spock: 'beats', paper: 'beats' },
-	spock: { scissors: 'beats', rock: 'beats' },
+	rock: ['scissors', 'lizard'],
+	paper: ['rock', 'spock'],
+	scissors: ['paper', 'lizard'],
+	lizard: ['spock', 'paper'],
+	spock: ['scissors', 'rock'],
 };
 
+// Game.prototype.winner = function() {
+//     if(this.PAIRS[this.player1.pick][this.player2.pick] === 'beats') return this.player1;
+//     if(this.PAIRS[this.player2.pick][this.player1.pick] === 'beats') return this.player2;
+//     if(this.player1.pick === this.player2.pick) return null;
+// };
 
 Game.prototype.winner = function() {
-    if(this.PAIRS[this.player1.pick][this.player2.pick] === 'beats') return this.player1;
-    if(this.PAIRS[this.player2.pick][this.player1.pick] === 'beats') return this.player2;
+    if(this.PAIRS[this.player1.pick].indexOf(this.player2.pick) > -1) return this.player1;
+    if(this.PAIRS[this.player2.pick].indexOf(this.player1.pick) > -1) return this.player2;
     if(this.player1.pick === this.player2.pick) return null;
 };
