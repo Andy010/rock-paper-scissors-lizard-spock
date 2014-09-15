84feb882c62d9e83c8b606b6c784eecd3e84a42c
Andy010 <andy_gates_010@hotmail.co.uk>
2014-09-15 16:36:17 +0100

    Adding spock and lizard

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -14,14 +14,16 @@ Player.prototype.picks = function(pick) {
 };
 
 Game.prototype.PAIRS = {
-	rock: 'scissors',
-	paper: 'rock',
-	scissors: 'paper',
+	rock: { scissors: 'beats', lizard: 'beats' },
+	paper: { rock: 'beats', spock: 'beats' },
+	scissors: { paper: 'beats', lizard: 'beats' },
+	lizard: { spock: 'beats', paper: 'beats' },
+	spock: { scissors: 'beats', rock: 'beats' },
 };
 
 
 Game.prototype.winner = function() {
-    if(this.PAIRS[this.player1.pick] === this.player2.pick) return this.player1;
-    if(this.PAIRS[this.player2.pick] === this.player1.pick) return this.player2;
+    if(this.PAIRS[this.player1.pick][this.player2.pick] === 'beats') return this.player1;
+    if(this.PAIRS[this.player2.pick][this.player1.pick] === 'beats') return this.player2;
     if(this.player1.pick === this.player2.pick) return null;
 };
